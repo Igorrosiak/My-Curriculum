cf2cac146798b5fc17f1a3174fae71f62d6912ff
Igorrosiak <igorluizbelterrosiak@gmail.com>
2023-01-19 16:23:31 -0300

    updating/introduction-structure-finished

--- my-curriculum/src/app/components/introduction/introduction-component.jsx
+++ my-curriculum/src/app/components/introduction/introduction-component.jsx
@@ -3,7 +3,32 @@ import "./introduction-component.scss"
 export const IntroductionComponent = props => {
   return (
     <main className="introduction-component">
-      <h1>Igor Rosiak, Desenvolvedor FullStack</h1>
+      <div className="left-container">
+        <h2>Igor Rosiak<strong>.</strong></h2>
+        <hr />
+        <p>17 anos, Estudante</p>
+        <button>Contact Me</button>
+      </div>
+
+      <div className="center-container">
+        <div className="circle">
+
+        </div>
+        <img src="#" alt="Imagem de Igor Rosiak" />
+      </div>
+
+      <div className="right-container">
+        <div>
+          <h3>Desenvolvedor FullStack</h3>
+          <hr />
+          <p>ReactJS | Java | Javascript</p>
+        </div>
+        <div>
+          <h3>Sobre Mim</h3>
+          <hr />
+          <p>Apaixonado por tecnologia, aspirante em estar entre os melhores e ser referência.</p>
+        </div>
+      </div>
     </main>
   );
 }
