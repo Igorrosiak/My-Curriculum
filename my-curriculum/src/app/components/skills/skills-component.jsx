02e0649c85163e62ee77f1d6d68e52f093e56248
Igorrosiak <igorluizbelterrosiak@gmail.com>
2023-01-19 16:14:18 -0300

    new/skills-component

---
+++ my-curriculum/src/app/components/skills/skills-component.jsx
@@ -0,0 +1,76 @@
+import "./skills-component.scss"
+
+export const SkillsComponent = props => {
+  return (
+    <main className="skills-component">
+      <div className="row">
+        <div>
+          <img src="#" alt="Image Icon ReactJS" />
+          <h4>ReactJS</h4>
+        </div>
+        <div>
+          <img src="#" alt="Image Icon Javascript" />
+          <h4>Javascript</h4>
+        </div>
+        <div>
+          <img src="#" alt="Image Icon Java" />
+          <h4>Java</h4>
+        </div>
+        <div>
+          <img src="#" alt="Image Icon PostgreSQL" />
+          <h4>PostgreSQL</h4>
+        </div>
+        <div>
+          <img src="#" alt="Image Icon MySQL" />
+          <h4>MySQL</h4>
+        </div>
+      </div>
+      
+      <div className="row">
+        <div>
+          <img src="#" alt="Image Icon Git" />
+          <h4>Git</h4>
+        </div>
+        <div>
+          <img src="#" alt="Image Icon Docker" />
+          <h4>Docker</h4>
+        </div>
+        <div>
+          <img src="#" alt="Image Icon Node" />
+          <h4>DBeaver</h4>
+        </div>
+        <div>
+          <img src="#" alt="Image Icon CSS" />
+          <h4>CSS</h4>
+        </div>
+        <div>
+          <img src="#" alt="Image Icon HTML" />
+          <h4>HTML</h4>
+        </div>
+      </div>
+
+      <div className="row">
+        <div>
+          <img src="#" alt="Image Icon Slack" />
+          <h4>Slack</h4>
+        </div>
+        <div>
+          <img src="#" alt="Image Icon Bitbucket" />
+          <h4>Bitbucket</h4>
+        </div>
+        <div>
+          <img src="#" alt="Image Icon Postman" />
+          <h4>Postman</h4>
+        </div>
+        <div>
+          <img src="#" alt="Image Icon Trello" />
+          <h4>Trello</h4>
+        </div>
+        <div>
+          <img src="#" alt="Image Icon Notion" />
+          <h4>Notion</h4>
+        </div>
+      </div>
+    </main>
+  );
+}
