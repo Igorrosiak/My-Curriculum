e29bfd3f31ca681f434af9b838376fdca887696f
Igor Rosiak <86728719+Igorrosiak@users.noreply.github.com>
2023-01-22 11:17:14 -0300

    chore

--- src/app/pages/home/home-page.jsx
+++ src/app/pages/home/home-page.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import "./home-styles";
+import * as S from "./home-styles";
 import { HeaderComponent } from "../../components/header/header-component";
 import { IntroductionComponent } from "../../components/introduction/introduction-component";
 import { SkillsComponent } from "../../components/skills/skills-component"
 import { WorksComponent } from "../../components/works/works-component"
 import { AboutMeComponent } from "../../components/about-me/about-me-component";
-import { Certificates } from "../../components/certificates/certificates-styles";
+import { CertificatesComponent } from "../../components/certificates/certificates-component";
 import { FooterComponent } from "../../components/footer/footer-component";
 
 export default class HomePage extends React.Component {
 
   render() {
     return (
-      <div className="home-page">
+      <S.Home>
         <HeaderComponent />
         <IntroductionComponent />
         <SkillsComponent />
         <WorksComponent />
         <AboutMeComponent />
-        <Certificates />
+        <CertificatesComponent />
         <FooterComponent />
-      </div>
+      </S.Home>
     );
   }
 }
