d5f1f6a9d3f114b2d031d64eeb43c6627c924ffc
Igorrosiak <igorluizbelterrosiak@gmail.com>
2023-02-04 05:17:05 -0300

    updating/works-structured

--- src/app/components/works/works-component.jsx
+++ src/app/components/works/works-component.jsx
@@ -1,10 +1,78 @@
 import * as S from "./works-styles";
+import seta from "../../../assets/seta.png"
 
 export const WorksComponent = props => {
   return (
     <S.Works>
-      <div></div>
-      <div></div>
+      <h2>Works</h2>
+      <hr />
+
+      <div className="box">
+        <div className="img">
+          <img src={seta} className="left-image" alt="" />
+        </div>
+        <div className="works">
+          <div className="work">
+            <div className="left-section">
+              <div className="content">
+                <img src="#" alt="Logo Dibba" />
+                <div>
+                  <h5>Dibba</h5>
+                  <p>Florianópolis, SC</p>
+                </div>
+              </div>
+              <ul className="tags">
+                <li>React</li>
+                <li>Java</li>
+                <li>Javascript</li>
+              </ul>
+            </div>
+            <div className="center-section">
+              <span>2022</span>
+              <span>-</span>
+              <span>Atualmente</span>
+            </div>
+            <div className="right-section">
+              <ul>
+                <li>- Desenvolvimento Web</li>
+                <li>- Desenvolvimento Backend</li>
+                <li>- Comunicação com time técnico</li>
+              </ul>
+            </div>
+          </div>
+          <div className="work">
+            <div className="left-section">
+              <div className="content">
+                <img src="#" alt="Logo Dibba" />
+                <div>
+                  <h5>Dibba</h5>
+                  <p>Florianópolis, SC</p>
+                </div>
+              </div>
+              <ul className="tags">
+                <li>React</li>
+                <li>Java</li>
+                <li>Javascript</li>
+              </ul>
+            </div>
+            <div className="center-section">
+              <span>2022</span>
+              <span>-</span>
+              <span>Atualmente</span>
+            </div>
+            <div className="right-section">
+              <ul>
+                <li>- Desenvolvimento Web</li>
+                <li>- Desenvolvimento Backend</li>
+                <li>- Comunicação com time técnico</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="img">
+          <img src={seta} alt="" />
+        </div>
+      </div>
     </S.Works>
   );
 }
--- src/app/components/works/works-styles.js
+++ src/app/components/works/works-styles.js
@@ -1,4 +1,114 @@
 import styled from "styled-components"
+import { themes } from "../../../utils/theme"
 
-export const Works = styled.main `
-  background-color: red;`
+export const Works = styled.div`
+  width: 100vw;
+  min-height: 65vh;
+  background-color: ${themes.colors.background_strong};
+  padding: 5vw;
+  margin: auto;
+  align-items: center;
+
+  h2{
+    text-align: center;
+    font-size: 2rem;
+  }
+
+  hr{
+    width: 10%;
+    color: ${themes.colors.purple_very_light};
+    border: 1.5px solid ${themes.colors.purple_very_light};
+    margin: auto;
+  }
+
+  div.box{
+    width: 100%;
+    display: flex;
+    padding-top: 5vh;
+
+    div.img{
+      width: 10%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      
+      img{
+        width: 40px;
+        height: 40px;  
+      }
+
+      img.left-image{
+        rotate: 180deg;
+      }
+    }
+
+    div.works{
+      width: 80%;
+
+      div.work{
+        margin-top: 1rem;
+        width: 100%;
+        background-color: ${themes.colors.background_light};
+        border-radius: 10px;
+        display: flex;
+        justify-content: space-between;
+
+        div.left-section{
+          display: flex;
+          flex-direction: column;
+          padding-left: 1.5em;
+
+          div.content{
+            padding-top: 1em;
+            display: flex;
+
+            img{
+              margin: 0.3rem;
+              width: 100px;
+              background-color: purple;
+            }
+
+            div{
+              padding-left: 1em;
+              display: flex;
+              flex-direction: column;
+              h5{
+                font-size: 2rem;
+              }
+              p{
+                font-size: 1rem;
+              }
+            }
+          }
+
+          ul.tags{
+            display: flex;
+            padding: 0;
+
+            li{
+              margin: 0.3rem;
+              padding: 0.2rem 0.75rem;
+              border: 1px solid ${themes.colors.gray};
+              border-radius: 10px;
+            } 
+          }
+        }
+
+        div.center-section{
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        div.right-section{
+          padding-right: 1.5em;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+      }
+    }
+  }
+`
