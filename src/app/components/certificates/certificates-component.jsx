482071fd934e0cddc77311d0d43fe9a54e76a24e
Igorrosiak <igorluizbelterrosiak@gmail.com>
2023-02-08 17:53:07 -0300

    updating/certificates

--- src/app/components/certificates/certificates-component.jsx
+++ src/app/components/certificates/certificates-component.jsx
@@ -1,10 +1,66 @@
+import { Carousel } from "react-bootstrap";
 import * as S from "./certificates-styles";
+import javascript from "../../../assets/certificadoJavascript.png"
+import html from "../../../assets/certificadoHtml.png"
+import html2 from "../../../assets/certificadoHtml2.png"
+import algoritmos from "../../../assets/certificadoAlgoritmos.png"
 
 export const CertificatesComponent = props => {
   return (
     <S.Certificates id="certificates">
-      <div></div>
-      <div></div>
+      <h2>Certificates</h2>
+      <hr />
+
+        <Carousel variant="dark" className="w-50">
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={javascript}
+              alt="Diploma Javascript"
+            />
+            <Carousel.Caption>
+              <h3>Certificado Javascript</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={html}
+              alt="Certificado "
+            />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={html2}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={algoritmos}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
     </S.Certificates>
   );
 }
--- src/app/components/certificates/certificates-styles.js
+++ src/app/components/certificates/certificates-styles.js
@@ -1,4 +1,36 @@
 import styled from "styled-components"
+import { themes } from "../../../utils/theme"
 
-export const Certificates = styled.main `
-  background-color: red;`
+export const Certificates = styled.div`
+  background-color: ${themes.colors.background_strong};
+  width: 100vw;
+  height: 80vh;
+  padding: 5vh;
+  margin: auto;
+  align-items: center;
+
+  h2{
+    text-align: center;
+    font-size: 2rem;
+  }
+
+  hr{
+    width: 10%;
+    color: ${themes.colors.purple_very_light};
+    border: 1.5px solid ${themes.colors.purple_very_light};
+    margin: auto;
+  }
+
+  div{
+    margin: auto;
+    padding-top: 1rem;
+  }
+  div.carousel-caption{
+    h3{
+      color: black !important;
+    }
+    p{
+      color: black !important;
+    }
+  }
+`
